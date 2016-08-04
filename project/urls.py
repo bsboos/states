9cb955068acd407bf1c4e56a19961fd59049548e
Your Name <you@example.com>
2016-08-04 11:52:33 +0300

    2nd

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+
 
 from app import views 
 
@@ -54,4 +56,4 @@ urlpatterns = [
     url(r'^detail/(?P<pk>\d+)/', 'app.views.detail'),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
